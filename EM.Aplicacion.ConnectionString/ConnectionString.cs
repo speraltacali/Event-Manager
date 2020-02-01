22db34be6553a3a1c6cd61350e88906cdcb52cd0
jose sabeckis flores <josesabeckis_pj@hotmail.com.ar>
2020-02-01 17:47:41 -0300

    primer cambio por jose definicion del login y implementacion del navbar

--- EM.Aplicacion.ConnectionString/ConnectionString.cs
+++ EM.Aplicacion.ConnectionString/ConnectionString.cs
@@ -4,15 +4,14 @@
     {
         private const string DataBase = "EventManagerDB";
 
-        private const string Server = @"DESKTOP-NK0OJF1";
+        private const string Server = @"DESKTOP-NDJOGL2\SQLEXPRESS";
 
         private const string User = "sa";
 
         private const string Password = "santi42175";
 
         public static string GetStringConection => $"Data Source={Server};" +
-                                                   $"Initial Catalog={DataBase};"+
-                                                   $"User Id={User};" +
-                                                   $"Password={Password};";
+                                                   $"Initial Catalog={DataBase};" +
+                                                   $"Integrated Security=true";
     }
 }
---
+++ EM.Presentacion.WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EM.Presentacion.WebAPI.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
