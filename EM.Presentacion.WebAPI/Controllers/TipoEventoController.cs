7101a2a2be7100c2392f7e6123ff0cd0532f40aa
jose sabeckis flores <josesabeckis_pj@hotmail.com.ar>
2020-02-07 20:47:24 -0300

    vista index y nuevo tipo de evento

---
+++ EM.Presentacion.WebAPI/Controllers/TipoEventoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EM.Presentacion.WebAPI.Controllers
+{
+    public class TipoEventoController : Controller
+    {
+        // GET: TipoEvento
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NuevoEvento()
+        {
+            return View();
+        }
+
+
+    }
+}
