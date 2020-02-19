60ffd5bb7e1e71e254232e67f1c9c01716d25a03
Santiago Agustin Peralta Cali <santiagoperaltacali@hotmail.com>
2020-02-19 00:46:55 -0300

    recibiendo los cambios.

---
+++ EM.Dominio.Entity/MetaData/IEventoView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EM.Dominio.Entity.MetaData
+{
+    public interface IEventoView
+    {
+        string Titulo { get; set; }
+
+        string Orante { get; set; }
+
+        string Organizacion { get; set; }
+
+        string Descripcion { get; set; }
+
+        string Mail { get; set; }
+
+        string Latitud { get; set; }
+
+        string Longitud { get; set; }
+
+        long TipoEventoId { get; set; }
+
+        byte[] Imagen { get; set; }
+
+        string Telefono { get; set; }
+
+        decimal Precio { get; set; }
+
+        long EntradaId { get; set; }
+
+        DateTime FechaEvento { get; set; }
+
+        DateTime HoraInicio { get; set; }
+
+        DateTime HoraFin { get; set; }
+
+
+        string DomicilioCompleto { get; set; }
+    }
+}
